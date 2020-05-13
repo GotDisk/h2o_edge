4d7cafa420a325e3989b308892e02b9069b6bc6f
GotDisk <phil_hummel@hotmail.com>
2020-05-13 03:05:24 +0000

    add dbplot raster

--- seq_eda.R
+++ seq_eda.R
@@ -1,6 +1,7 @@
 library(readr)
 library(corrr)
 library(ggplot2)
+library(dbplot)
 
 train_df <- read.csv("data/sequence.csv", header = FALSE, sep=",")
 
@@ -8,7 +9,11 @@ corm <- cor(train_df)
 
 train_comp <- prcomp(train_df)
 
-plot_2d <- ggplot(train_df, aes(x=V1, y=V2)) +
+plot_2d <- ggplot(train_df, aes(x=V1, y=V3)) +
   geom_point() +
   stat_density2d()
 
+dbplot_raster(train_df, V1, V2, resolution = 16)
+
+train_lm <- lm(V1~V3,data = train_df)
+summary(train_lm)
